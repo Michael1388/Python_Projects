bd376af795ea0ae3041cc90fa1166a9d67a929b7
Michael1388 <119345695+Michael1388@users.noreply.github.com>
2023-01-30 18:48:33 -0600

    Update Abstraction Exercises

---
+++ Exercises/Encapsulation/Abstraction.py
@@ -0,0 +1,32 @@
+
+# Abstraction: 
+# Data abstraction hides the complex 
+# details of items while only revealing the essential or relevant data.
+
+
+
+from abc import ABC, abstractmethod
+
+class car(ABC):
+    def paySlip(self,amount):
+        print("Your purchase amount: ", amount)
+    
+# this function is telling us to pass in an argument, but we wont  
+# define how or what kind of data it will be.
+
+    @abstractmethod
+    def payment(self, amount):
+        pass
+
+class DebitCardPayment(car):
+# here we've defined how to implement the payment function from its parent
+# paySlip class.
+    def payment(self,amount):
+        print('Your purchase amount of {} has exceeded your $100 limit '.format(amount))
+
+obj = DebitCardPayment()
+obj.paySlip("$400") 
+obj.payment("$400")
+
+
+
