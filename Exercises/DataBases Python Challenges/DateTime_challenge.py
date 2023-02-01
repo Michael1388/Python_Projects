ea390a42cc51c2549ed7e5c48a0fd0a4a14f7359
Michael1388 <119345695+Michael1388@users.noreply.github.com>
2023-02-01 10:08:45 -0600

    Add DateTime Challenge

--- Exercises/DataBases Python Challenges/DateTime_challenge.py
+++ Exercises/DataBases Python Challenges/DateTime_challenge.py
@@ -15,12 +15,53 @@
 #  
 # 3.Print out to the screen the three branches and whether they are 
 # open or closed.
+#  
+
 
 import datetime
 import pytz
 
+"""import pytz
 
-# prints the now time on my machine
-#x = datetime.datetime.now()
-#print(x)
+print('Timezones')
+for timeZone in pytz.all_timezones:
+    print(timeZone)"""
+
+# ("%Y:%m:%d %H:%M:%S %Z %z")
+# UTC timezone Datetime
+dt_local = datetime.datetime.now(pytz.utc)
+# print("UTC DateTime:", dt_local.strftime("%Y:%m:%d %H:%M %p"))
+
+# convert UTC timezone to 'US/Pacific'
+dt_us_pacific = dt_local.astimezone(pytz.timezone('US/Pacific'))
+print("\nUS Portland Branch HQ hours are 9am to 5pm: \nCurrent time is:", dt_us_pacific.strftime("%I:%M %p")) # prints local time
+
+portlandHour = int(dt_us_pacific.strftime("%H"))
+
+if portlandHour >= 9 and portlandHour <= 17:
+    print("Portland HQ Branch is OPEN\n")
+else: 
+    print('Portland HQ Branch is CLOSED\n')
+
+# Convert 'US/Pacific' timezone to US/Eastern (America/New_York)
+dt_us_eastern = dt_us_pacific.astimezone(pytz.timezone('America/New_York'))
+print("US New York Branch hours are 9am to 5pm: \nCurrent time is:", dt_us_eastern.strftime("%I:%M %p"))
+
+newyorkHour = int(dt_us_eastern.strftime("%H"))
+
+if newyorkHour >= 9 and newyorkHour <= 17:
+    print("New York Branch is OPEN\n")
+else: 
+    print("New York Branch is CLOSED\n")
+
+# Convert US/Eastern timezone to London (Europe/London) timezone
+dt_euro_lon = dt_us_eastern.astimezone(pytz.timezone('Europe/London'))
+print("UK London Branch hours are 9am to 5pm: \nCurrent time is:", dt_euro_lon.strftime("%I:%M %p"))
+
+londonHour = int(dt_euro_lon.strftime("%H"))
+
+if londonHour >= 9 and londonHour <= 17:
+    print("London Branch is OPEN\n")
+else: 
+    print('London Branch is CLOSED\n')
 
--- Exercises/SANDBOX.py
+++ Exercises/SANDBOX.py
@@ -9,3 +9,34 @@ num2 = 1
 answer = "The answer is {}.".format(num1 + num2)
 print(answer)
 
+
+
+import datetime
+import pytz
+
+
+# prints the now time on my machine
+#x = datetime.datetime.now()
+#print(x)
+
+if dt_euro_lon.strftime("%Y:%m:%d %H:%M:%S %Z %z") > 09:00 
+
+else: 
+    print('closed')
+
+    ("%Y:%m:%d %H:%M:%S %Z %z")
+
+>= 9 == 9
+> 9 == 10
+
+<= 5 == 5
+< 5 == 4 
+
+    if londonHour >= 9:
+    if londonHour <= 5:
+        print("open")
+
+if londonHour >= 9:
+    print("open")
+elif londonHour <= 5:
+    print("open")
