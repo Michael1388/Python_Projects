12e9349d55f5bd3bc6b102a1230aae51ecf62e9e
Michael1388 <119345695+Michael1388@users.noreply.github.com>
2023-02-01 18:36:10 -0600

    Python File Transfer Project

--- Exercises/SANDBOX.py
+++ Exercises/SANDBOX.py
@@ -40,3 +40,21 @@ if londonHour >= 9:
     print("open")
 elif londonHour <= 5:
     print("open")
+
+            for i in source_files:
+            # checks source file time stamp
+            fileStamp = os.path.getmtime(source + '/' + i)
+            #print(fileStamp)
+            #print(datetime.datetime.fromtimestamp(fileStamp).strftime("%H:%M %S")) #testing my code
+            fileStampDate = datetime.datetime.fromtimestamp(fileStamp)
+            current = datetime.datetime.now()
+            #print("Now: " +  str(current.strftime("%H:%M %S"))) #testing code
+            yesterday = current - timedelta(days = 1)
+            if (fileStampDate >= yesterday):
+                # moves each file from the source to the destination
+                shutil.move(source + '/' + i, destination)
+                print(i + ' was successfully transferred.')
+            #added extra feature to see files that were not transferred to validate differences
+            # but can see where in a folder of hundreds of files this would not be desirable 
+            else:
+                print(i + ' was not transfered') 
